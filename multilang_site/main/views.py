06a444f9da5cca3df397ebc7aaeeb71f75db94bc
Nolan L. <nonocraft28@gmail.com>
2024-06-16 13:30:00 +0200

    Internationalisation et Interface Utilisateur

--- multilang_site/main/views.py
+++ multilang_site/main/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
+from django.utils.translation import gettext as _
 
 def home(request):
     return render(request, 'main/home.html')
 
-
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'main/article_list.html', {'articles': articles})
+    
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'main/article_detail.html', {'article': article})
--- multilang_site/multilang_site/urls.py
+++ multilang_site/multilang_site/urls.py
@@ -1,26 +1,16 @@
-"""
-URL configuration for multilang_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+
 from main import views
 
-# GIVES A URL PATH TO THE PAGE IT SHOULD MANAGE
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += i18n_patterns(
     path('', views.home, name='home'),
     path('articles/', views.article_list, name='article_list'),
-]
+    path('article/<int:article_id>/', views.article_detail, name='article_detail'),
+    path('i18n/', include('django.conf.urls.i18n')),
+)
